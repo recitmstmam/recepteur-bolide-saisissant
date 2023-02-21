7d3e0670021022122dbddc8ab5fd9b3bf5c7665c
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2023-02-21 12:44:02 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    t1 = input.runningTimeMicros() / 1000000
+    d1 = receivedNumber / 20 * 157.08 / 100
+    Vitesse = Math.abs(d2 - d1) / Math.abs(t2 - t1)
+    serial.writeValue("vi", Vitesse)
+    t2 = t1
+    d2 = d1
+    led.toggle(0, 0)
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendString("Go")
     basic.pause(100)
     control.reset()
+    basic.showIcon(IconNames.Yes)
 })
+let t2 = 0
+let d2 = 0
+let Vitesse = 0
+let d1 = 0
+let t1 = 0
 radio.setGroup(1)
 let Fentes = 0
-let t1 = 0
-let d1 = 0
-basic.forever(function () {
-	
-})
+t1 = 0
+d1 = 0
+basic.showIcon(IconNames.No)
