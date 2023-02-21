dba0aa1621a51683dab2e0719094759a4e450528
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2023-02-21 11:07:28 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Go")
+    basic.pause(100)
+    control.reset()
+})
+radio.setGroup(1)
+let Fentes = 0
+let t1 = 0
+let d1 = 0
+basic.forever(function () {
+	
+})
